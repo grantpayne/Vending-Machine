336ccd15b2916c7180898dca158647b0548d6f29
Abby Bougher <abbybougher@gmail.com>
2019-02-04 15:51:14 -0500

    01/17 File IO Reading Complete

--- 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -13,36 +13,6 @@ namespace file_io_part1_exercises_pair
             string filePath = Console.ReadLine();
 
             GetFile(filePath);
-
-
-
-
-
-            //// Wrap the effort in a try-catch block to handle any exceptions
-            //try
-            //{
-            //    //Open a StreamReader with the using statement
-            //    using (StreamReader sr = new StreamReader(fullPath))
-            //    {
-            //        // Read the file until the end of the stream is reached
-            //        // EndOfStream is a "marker" that the stream uses to determine 
-            //        // if it has reached the end
-            //        // As we read forward the marker moves forward like a typewriter.
-            //        while (!sr.EndOfStream)
-            //        {
-            //            // Read in the line
-            //            string line = sr.ReadLine();
-
-            //            // Print it to the screen
-            //            Console.WriteLine(line);
-            //        }
-            //    }
-            //}
-            //catch (IOException e) //catch a specific type of Exception
-            //{
-            //    Console.WriteLine("Error reading the file");
-            //    Console.WriteLine(e.Message);
-            //}
         }
         public static void GetFile(string filePath)
         {
@@ -50,35 +20,40 @@ namespace file_io_part1_exercises_pair
             {
                 using (StreamReader reader = new StreamReader(filePath))
                 {
-                    int wordCounter = 0;
+                    //int wordCounter = 0;
                     int sentenceCounter = 0;
                     int lineCounter = 0;
+                    List<string> words = new List<string>();
                     while (!reader.EndOfStream)
                     {
                         lineCounter++;
                         // Read in the line
                         string line = reader.ReadLine();
+                        //This is counting the sentences in the text file:
                         for (int i = 0; i < line.Length; i++)
                         {
                             if ((line.Length > 0) && (line[i] == '.' || line[i] == '!' || line[i] == '?'))
                             {
                                 sentenceCounter++;
-
-                                //if (line[i + 1] == '.' || line[i + 1] == '!' || line[i + 1] == '?')
-                                //{
-                                //    i++;
-
-                                //}
                             }
                         }
-                        //if ((line.Length > 0) && (line[line.Length - 1] == '.' || line[line.Length - 1] == '!' || line[line.Length - 1] == '?'))
-                        //{
-                        //    sentenceCounter++;
-                        //}
-                        // Print it to the screen
-                        Console.WriteLine(line);
+                        //This is counting the words in the text file:
+                        string[] wordArray = line.Split(new char[] { ' ', '.', '!', '?', '\n' });
+                        words.AddRange(wordArray);
+                        for (int i = 0; i < words.Count; i++)
+                        {
+                            if (String.IsNullOrEmpty(words[i]))
+                            {
+                                words.RemoveAt(i);
+                            }
+                        }
+                       
+
+
+
                     }
-                    Console.WriteLine(sentenceCounter);
+                    Console.WriteLine("Word count: " + words.Count);
+                    Console.WriteLine("Sentence count: " + sentenceCounter);
                 }
             }
             catch (IOException ex) //catch a specific type of Exception
