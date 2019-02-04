8f37739a9baba05274e49e7559612c32c2ad11bf
Abby Bougher <abbybougher@gmail.com>
2019-02-04 15:22:42 -0500

    Sentence count complete.

--- 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +8,86 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ask user for filepath:
+            Console.WriteLine("What is the fully qualified filepath of the file?");
+            string filePath = Console.ReadLine();
+
+            GetFile(filePath);
+
+
+
+
+
+            //// Wrap the effort in a try-catch block to handle any exceptions
+            //try
+            //{
+            //    //Open a StreamReader with the using statement
+            //    using (StreamReader sr = new StreamReader(fullPath))
+            //    {
+            //        // Read the file until the end of the stream is reached
+            //        // EndOfStream is a "marker" that the stream uses to determine 
+            //        // if it has reached the end
+            //        // As we read forward the marker moves forward like a typewriter.
+            //        while (!sr.EndOfStream)
+            //        {
+            //            // Read in the line
+            //            string line = sr.ReadLine();
+
+            //            // Print it to the screen
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //catch (IOException e) //catch a specific type of Exception
+            //{
+            //    Console.WriteLine("Error reading the file");
+            //    Console.WriteLine(e.Message);
+            //}
+        }
+        public static void GetFile(string filePath)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    int wordCounter = 0;
+                    int sentenceCounter = 0;
+                    int lineCounter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        lineCounter++;
+                        // Read in the line
+                        string line = reader.ReadLine();
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            if ((line.Length > 0) && (line[i] == '.' || line[i] == '!' || line[i] == '?'))
+                            {
+                                sentenceCounter++;
+
+                                //if (line[i + 1] == '.' || line[i + 1] == '!' || line[i + 1] == '?')
+                                //{
+                                //    i++;
+
+                                //}
+                            }
+                        }
+                        //if ((line.Length > 0) && (line[line.Length - 1] == '.' || line[line.Length - 1] == '!' || line[line.Length - 1] == '?'))
+                        //{
+                        //    sentenceCounter++;
+                        //}
+                        // Print it to the screen
+                        Console.WriteLine(line);
+                    }
+                    Console.WriteLine(sentenceCounter);
+                }
+            }
+            catch (IOException ex) //catch a specific type of Exception
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
