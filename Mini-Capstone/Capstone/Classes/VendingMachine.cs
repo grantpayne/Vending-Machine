cfe11bab3e07838bc06df8ac6d53fc949874e10a
Kat Humphries <katcancode@gmail.com>
2019-02-07 13:59:10 -0500

    stubbed, need io

--- Mini-Capstone/Capstone/Classes/VendingMachine.cs
+++ Mini-Capstone/Capstone/Classes/VendingMachine.cs
@@ -9,5 +9,28 @@ namespace Capstone.Classes
         private List<VendingMachineItem> items = new List<VendingMachineItem>(); //do not change
         private string filePath = @"C:\VendingMachine"; //do not change  - write all files/reports here...
 
+        public decimal TransactionBalance { get; private set; }
+        public decimal TotalRevenue { get; private set; }
+
+
+        public void Vend(string itemSelection)
+        {
+            
+            //dispense item if >0
+            //adjust inventory VendingMachineItem.Count -1
+
+        }
+
+        public int []  DispenseChange()
+        {   //use vendingMachine.TransactionBalance and break into coins.
+            //figure out quarters,nickels,dimes
+            int [] change = new int [3]; //quarter, nickel, dimes
+                                         //greedy algo?
+            TransactionBalance = 0;
+            return change;
+        }
+        
+
     }
+
 }
