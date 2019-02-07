1360056b716c72e89ede618ee94943082849843a
Kat Humphries <katcancode@gmail.com>
2019-02-07 15:48:26 -0500

    updated UI and Vending

--- Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -13,10 +13,49 @@ namespace Capstone.Classes
             bool done = false;
             while (!done)
             {
-                Console.WriteLine("This is the UserInterface");
-                Console.ReadLine();
+                done = Menu();
             }
+        }
+        public bool Menu()
+        {
+            bool done = false;
+            //menu goes here
+            return done;
+        }
+
+        public void Display()
+        {
+            //
+        }
+
+        public void Purchase()
+        {
+            int moneyTendered =0;
+            //while false prompt for non counterfit money
+            vendingMachine.AddTender(moneyTendered);
+           
+           string itemSelection ="";
+                       
+            //check transaction balance
+           //check if exist 
+            vendingMachine.Vend(itemSelection);
+
+            
+
+            //while done tendering and selecting
+            vendingMachine.DispenseChange();  //display change from decimal [] 
 
         }
+        
+
+        public void OptionalReport()
+        {
+
+        }
+        public void emptyMachine()
+        {
+
+        }
+
     }
 }
