b344048b362952f552470b45b59dec1cbff06d55
Kat Humphries <katcancode@gmail.com>
2019-02-10 12:58:45 -0500

    Refactored, removed magic constants and cleaned up UI

--- Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -8,208 +8,190 @@ namespace Capstone.Classes
     public class UserInterface
     {
         public VendingMachine vendingMachine = new VendingMachine();
-
         public void RunInterface()
         {
             bool done = false;
-
-
-
             while (!done)
             {
-
                 done = Menu();
             }
-        }
+        }//RunInterface
         public bool Menu()
         {
-            bool isPurchaseTransactionComplete = false;
             Console.Clear();
             //TODO: Underline current menu title
             Console.WriteLine("Welcome to Fulton Vending!\n\nMAIN MENU\n\nPlease make a selection:\n\n(1) Display Vending Machine Items\n(2) Purchase\n(3) End\n");
             string menuInput = Console.ReadLine();
-            if (menuInput == "1") // Magic Constant test
-            {
+           
+            //TODO: KH Done Magic Constant for menu
+            const bool StopMenu = false;
+            const bool ContinueMenu = true;
+            //const bool Complete = true;
+            const bool Incomplete = false;
+            bool isPurchaseTransactionComplete = Incomplete;
+            const string DisplaySelction = "1";
+            const string PurchaseSelection = "2";
+            const string SecretReport = "9";
+            const string EndProgram = "3";
 
+            if (menuInput == DisplaySelction)
+            {
                 Display();
                 Console.WriteLine("\nPress enter to return to the MAIN MENU...");
                 Console.ReadLine();
-                return false; // return discontinueMenu
+                return StopMenu;
             }
-            else if (menuInput == "2") // Magic Constant test
+            else if (menuInput == PurchaseSelection)
             {
-                while (!isPurchaseTransactionComplete)
+                while (isPurchaseTransactionComplete == Incomplete)
                 {
                     isPurchaseTransactionComplete = Purchase();
-
                 }
-                return false; // return discontinueMenu
-
+                return StopMenu;
             }
-            else if (menuInput == "3") // Magic Constant test
+            else if (menuInput == EndProgram)
             {
                 Console.WriteLine("Press enter to exit.");
                 Console.ReadLine();
-                return true; // return continueMenu
+                return ContinueMenu;
             }
-            else if (menuInput == "9") // Magic Constant test
+            else if (menuInput == SecretReport)
             {
                 Console.WriteLine("Generating summary report...");
                 OptionalReport();
                 Console.ReadLine();
-                return false; // return discontinueMenu
+                return StopMenu;
             }
             else
             {
                 Console.WriteLine("Invalid selection - press enter to return to the main menu.");
                 Console.ReadLine();
-                return false; // return discontinueMenu
+                return StopMenu;
             }
-
-        }
-
-
+        }//Menu
         public void Display()
         {
             Console.Clear();
-
             List<VendingMachineItem> items = vendingMachine.GetInventoryData();
             List<string> itemsDisplay = new List<string>();
-
             foreach (VendingMachineItem item in items)
             {
                 itemsDisplay.Add(item.ToString());
             }
             itemsDisplay.Sort();
-
             Console.WriteLine(string.Format("{0, 5} {1, 25} {2, 8:C} {3, 9}\n", "Code", "Product", "Price", "Quantity"));
-
             foreach (string item in itemsDisplay)
             {
-
                 Console.WriteLine(item);
             }
-
-            //Console.WriteLine("\nPress enter to return to the MAIN MENU...");
-            //Console.ReadLine();
-            //5. When the customer selects ​(1) Display Vending Machine Items​ they have presented a list of all items in the vending machine, the price, and its quantity remaining. a. Each vending machine product has a slot identifier and a purchase price. b. Each slot in the vending machine has enough room for 5 of that product. c. Every product is initially stocked to the maximum amount. d. A product which has run out should indicate it is SOLD OUT. 
-        }
-
+        }//Display
         public bool Purchase()
         {
-            bool isPurchaseTransactionComplete = false;
+            const bool PurchaseComplete = true;
+            const bool PurchaseIncomplete = false;
+            
+
             string userSelection = "";
+            const string FeedMoneyOption = "1";
+            const string SelectProductOption = "2";
+            const string FinishTransactionOption = "3";
 
             // might want to consider separate method for testing valid user selection
-            while (!(userSelection == "1" || userSelection == "2" || userSelection == "3")) // Magic Constant test
+            while (!(userSelection == FeedMoneyOption || userSelection == SelectProductOption || userSelection == FinishTransactionOption)) // Magic Constant test
             {
                 Console.Clear();
                 Console.WriteLine("PURCHASE MENU\n\nPlease make a selection:\n\n(1) Feed Money\n(2) Select Product\n(3) Finish Transaction\n");
                 Console.WriteLine("Current Money Provided: {0:C}\n", vendingMachine.TransactionBalance);
                 userSelection = Console.ReadLine();
-
+            }
+            if (userSelection == FeedMoneyOption)
+            {
+                FeedMoney();
             }
 
-            // example
-            // if ( feedMoneyOption() )
-            //     feedMoneyLogic();
-            // else if ( vendOption() )
-            //     vendLogic();
-            // else if ( dispenseChangeOption() )
-            //     dispenseChangeLogic();
-            if (userSelection == "1") // Magic Constant test
+            else if (userSelection == SelectProductOption)
             {
-
-                bool isAccepted = false;
-                Console.Clear();
-                Console.WriteLine("Please enter dollar bill to deposit (U.S. currency only)");
-                int dollarFeed = 0;
-
-                int.TryParse(Console.ReadLine(), out dollarFeed);
-                isAccepted = vendingMachine.AddTender(dollarFeed);
-
-                if (!isAccepted)
-                {
-                    Console.WriteLine("Valid U.S. currency ONLY\n");
-                    Console.ReadLine();
-                    isAccepted = true; // Is this being used anywhere now? There is no loop right now.
-                }
-            }
-            else if (userSelection == "2") // Magic Constant test
-            {
-                Display();
-                Console.WriteLine("\nPlease select product code: ");
-                string prodcutSelector = Console.ReadLine(); // TODO Fix prodcutSelector spelling
-
-                string transactionResult = vendingMachine.Vend(prodcutSelector);
-                if (transactionResult == "SOLD")//Magic Sold
-
-                {
-
-
-                    List<VendingMachineItem> products = new List<VendingMachineItem>(vendingMachine.GetInventoryData());
-                    foreach (VendingMachineItem item in products)
-                    {
-                        if (item.SlotID == prodcutSelector)
-                        {
-                            Console.WriteLine(item.Message);
-                        }
-                    }
-                    Console.ReadLine();
-                    userSelection = "";
-
-
-                }
-                else
-                {
-                    if (transactionResult == "DoesNotExist") // Magic Constant test
-                    {
-                        Console.WriteLine("Item does not exist! Select a valid product code!");
-
-                    }
-                    else if (transactionResult == "OutOfStock") // Magic Constant test
-                    {
-                        Console.WriteLine("OUT OF STOCK! Please make another selection.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter more money or make another selection.");
-                    }
-
-
-                    Console.ReadLine();
-                    userSelection = "";
-
-                }
-
-
+                SelectProduct();
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine($"Your change is {vendingMachine.TransactionBalance.ToString("C")}");
-                int [] change = vendingMachine.DispenseChange();  //display change from decimal [] 
-                Console.WriteLine($"{change[0]} quarters, {change[1]} dimes, {change[2]} nickels");
-                Console.ReadLine();
-                isPurchaseTransactionComplete = true;
-                //return PurchaseTransactionComplete (true)//add this at top to avoid magic numbers.
-            }
-
-
-
-
-            return isPurchaseTransactionComplete;  //purchaseTranscationIncomplete = false;
-        }
-
-
+                FinishTransaction();
+                return PurchaseComplete;
+            }//finished
+            return PurchaseIncomplete; 
+        } //Purchase
         public void OptionalReport()
-        {
+{
 
-        }
-        public void emptyMachine()
-        {
+}//OptionalReport
+        public void EmptyMachine()
+{
 
-        }
+}//EmptyMachine
 
+        public void FeedMoney()
+{
+    const bool Counterfeit = false;
+    bool isFeedMoneyAccepted = Counterfeit;
+
+    Console.Clear();
+    Console.WriteLine("Please enter dollar bill to deposit (U.S. currency only)");
+    int dollarFeed = 0;
+    int.TryParse(Console.ReadLine(), out dollarFeed);
+    isFeedMoneyAccepted = vendingMachine.AddTender(dollarFeed);
+
+    if (isFeedMoneyAccepted == Counterfeit)
+    {
+        Console.WriteLine("Valid U.S. currency ONLY\n");
+        Console.ReadLine();
     }
-}
+}//FeedMoney
+        public void SelectProduct()
+        {
+            Display();
+
+            Console.WriteLine("\nPlease select product code: ");
+            string productSelector = Console.ReadLine(); // TODO KH Done Fix productSelector spelling
+             
+            string transactionResult = vendingMachine.Vend(productSelector);
+
+            if (transactionResult == "SOLD")//Magic Sold
+            {
+                List<VendingMachineItem> products = new List<VendingMachineItem>(vendingMachine.GetInventoryData());
+                foreach (VendingMachineItem item in products)
+                {
+                    if (item.SlotID.ToUpper() == productSelector.ToUpper()) 
+                    {
+                        Console.WriteLine(item.Message);
+                    }
+                }
+              
+            }
+            else if (transactionResult == "DoesNotExist") // Magic Constant test
+            {
+                Console.WriteLine("Item does not exist! Select a valid product code!");
+            }
+            else if (transactionResult == "OutOfStock") // Magic Constant test
+            {
+                Console.WriteLine("OUT OF STOCK! Please make another selection.");
+            }
+            else //cant afford
+            {
+                Console.WriteLine("Please enter more money or make another selection.");
+            }
+            Console.ReadLine();
+            
+           
+        }//SelectProduct
+        public void FinishTransaction()
+        {
+
+        Console.Clear();
+        Console.WriteLine($"Your change is {vendingMachine.TransactionBalance.ToString("C")}");
+        int[] change = vendingMachine.DispenseChange();  //display change from decimal [] 
+        Console.WriteLine($"{change[0]} quarters, {change[1]} dimes, {change[2]} nickels");
+        Console.ReadLine();
+        }//FinishTransaction
+
+        }//class
+}//namespace
--- Mini-Capstone/Capstone/Classes/VendingMachine.cs
+++ Mini-Capstone/Capstone/Classes/VendingMachine.cs
@@ -11,24 +11,24 @@ namespace Capstone.Classes
         private List<VendingMachineItem> items = new List<VendingMachineItem>(); //do not change
         private string filePath = @"C:\VendingMachine"; //do not change  - write all files/reports here...
         private string fileName = @"vendingmachine.csv";
-
         public decimal TransactionBalance { get; private set; }
         public decimal TotalRevenue { get; private set; }
-
         IO io = new IO();
+
         public VendingMachine()
         {
             TransactionBalance = 0M;
             TotalRevenue = 0M;
-
             List<string[]> stockList = new List<string[]>();
             stockList = io.FetchData(filePath, fileName);
 
-            //TODO: refactor stockItemArray indexing variables to specific names
-
+            //TODO: KH Done refactor stockItemArray indexing variables to specific names
             foreach (string[] stockItemArray in stockList)
             {
-                items.Add(new VendingMachineItem(stockItemArray[0], stockItemArray[1], decimal.Parse(stockItemArray[2]))); //TODO: Handle Parse exception
+                string slotId = (stockItemArray[0]);
+                string itemName = (stockItemArray[1]);
+                decimal price = decimal.Parse(stockItemArray[2]);//TODO: Handle Parse exception
+                items.Add(new VendingMachineItem(slotId, itemName, price)); 
             }
 
         }
@@ -39,115 +39,84 @@ namespace Capstone.Classes
             //checked for dollars bills {1, 2, 5, 10, 20}
             List<int> allowedBills = new List<int> { 1, 2, 5, 10, 20 };
             bool isAcceptedUSBill = allowedBills.Contains(moneyTendered);
+
             if (isAcceptedUSBill)
             {
                 TransactionBalance += moneyTendered;
                 io.WriteLog(filePath, "FEED MONEY:", moneyTendered, TransactionBalance);
-                return isAcceptedUSBill; // magic constant
+                return isAcceptedUSBill; 
             }
             else
             {
-                return isAcceptedUSBill; // magic constant
+                return isAcceptedUSBill; 
             }
-            // if good update log
-            //update TransactionBalance
-            //return true;
-            //false if coutnerfit
+          
         }
 
         public string Vend(string itemSelection)
         {
-            
-
-             string result = "";
+            //TODO KH ADDED CONST FOR STATUS
+            const string sold = "SOLD";
+            const string outOfStock = "OutOfStock";
+            const string notASlotID = "DoesNotExist";
+            const string cantAfford = "cantAfford";
+            string result = "";
 
             foreach (VendingMachineItem item in items)
             {
-                if (item.SlotID == itemSelection)
+                if (item.SlotID.ToUpper() == itemSelection.ToUpper())
                 {
                     if (item.InventoryCount > 0)
                     {
-
-
                         if (TransactionBalance >= item.Price)
                         {
-
-
-
                             decimal initialBalance = TransactionBalance;
-
                             item.InventoryCount--;
                             TransactionBalance -= item.Price;
                             TotalRevenue += item.Price;
-
-                            
                             // pre-balance for IO call = TransactionBalance + item.Price;
-                            
-
-                           io.WriteLog(filePath, (item.ItemName + " " + item.SlotID), (TransactionBalance + item.Price), TransactionBalance);
-
-                            result = "SOLD"; //TODO// Magic Constant? make property 
-
+                            io.WriteLog(filePath, (item.ItemName + " " + item.SlotID), (TransactionBalance + item.Price), TransactionBalance);
+                            result = sold; 
                             break;
-
-
-
-
-
                         }
                         else
                         {
-                            result = "cantAfford"; // Magic Constant
+                            result = cantAfford; 
                             break;
                         }
-
-
                     }
                     else
                     {
-                        result = "OutOfStock"; // Magic Constant
+                        result = outOfStock;
                         break;
-                    }
-                                                          
+                    }                                                  
                 }
                 else
                 {
-                    result = "DoesNotExist"; // Magic Constant
+                    result = notASlotID; 
                 }
             }
-
-
-
             return result;
         }
-
 
         public int[] DispenseChange()
         {
             // Assume all prices are divisible by 5 cents because there are no pennies.
-            // Figure out quarters[0], dimes[1], nickels[2]
-
+            // quarters[0], dimes[1], nickels[2]
             decimal tempBalance = TransactionBalance;
             int[] change = new int[3];
-
             change[0] = (int)Math.Floor(TransactionBalance / 0.25M);
             TransactionBalance -= change[0] * 0.25M;
-
             change[1] = (int)(Math.Floor(TransactionBalance / 0.10M));
             TransactionBalance -= change[1] * 0.10M;
-
             change[2] = (int)(Math.Floor(TransactionBalance / 0.05M));
-
             TransactionBalance = 0;
             io.WriteLog(filePath, "GIVE CHANGE:", tempBalance, TransactionBalance);
-
             return change;
-
-        }
+            }
 
         public List<VendingMachineItem> GetInventoryData()
         {
-
             return items;
         }
 
